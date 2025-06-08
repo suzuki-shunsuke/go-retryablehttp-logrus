5c6dfcb9b90fe8f81f2d8655988066123db24b5d
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-06-08 20:26:29 +0900

    feat: implement basic function

---
+++ rlog/log.go
@@ -0,0 +1,50 @@
+package rlog
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Entry interface {
+	WithFields(fields logrus.Fields) *logrus.Entry
+}
+
+type Logger struct {
+	entry Entry
+}
+
+func New(entry Entry) *Logger {
+	return &Logger{
+		entry: entry,
+	}
+}
+
+func (l *Logger) Debug(msg string, keysAndValues ...any) {
+	l.entry.WithFields(createFields(keysAndValues)).Debug(msg)
+}
+
+func (l *Logger) Error(msg string, keysAndValues ...any) {
+	l.entry.WithFields(createFields(keysAndValues)).Error(msg)
+}
+
+func (l *Logger) Info(msg string, keysAndValues ...any) {
+	l.entry.WithFields(createFields(keysAndValues)).Info(msg)
+}
+
+func (l *Logger) Warn(msg string, keysAndValues ...any) {
+	l.entry.WithFields(createFields(keysAndValues)).Warn(msg)
+}
+
+func createFields(keysAndValues []any) logrus.Fields {
+	s := len(keysAndValues)
+	if s%2 != 0 {
+		s--
+	}
+	cnt := s / 2
+	fields := make(logrus.Fields, cnt)
+	for i := range cnt {
+		fields[fmt.Sprint(keysAndValues[i*2])] = keysAndValues[i*2+1]
+	}
+	return fields
+}
